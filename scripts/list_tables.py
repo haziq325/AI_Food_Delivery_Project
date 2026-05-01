ed8760bc53326457f5e00b5ad872eabce5a18b92
sarim8133 <smsarim6@gmail.com>
2026-05-01 17:00:01 +0500

    Update list_tables.py

--- scripts/list_tables.py
+++ scripts/list_tables.py
@@ -1,27 +1,68 @@
-from db_manager import DatabaseManager
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+model = DiscreteBayesianNetwork([
+(&#39;Education&#39;, &#39;Interview&#39;),
+(&#39;Experience&#39;, &#39;Interview&#39;),
+(&#39;Interview&#39;, &#39;HiringDecision&#39;)
+])
 
-def verify_setup():
-    db = DatabaseManager()
-    
-    # Query to list all user-created tables
-    query = """
-        SELECT table_name 
-        FROM information_schema.tables 
-        WHERE table_schema = 'public';
-    """
-    
-    tables = db.fetch_data(query)
-    
-    print("\n---  Database Table List ---")
-    if tables:
-        for table in tables:
-            print(f"        Found Table:        {table[0]}")
-    else:
-        print(" No tables found. check your setup of corect file or database")
+# Education
+cpd_education = TabularCPD(
+variable=&#39;Education&#39;,
+variable_card=2,
+values=[[0.65], [0.35]],
+state_names={&#39;Education&#39;: [&#39;High&#39;, &#39;Low&#39;]}
+)
+# Experience
+cpd_experience = TabularCPD(
+variable=&#39;Experience&#39;,
+variable_card=2,
+values=[[0.5], [0.5]],
+state_names={&#39;Experience&#39;: [&#39;Experienced&#39;, &#39;Inexperienced&#39;]}
+)
 
-    print("------------------------------\n")
-    
-    db.close()
-
-if __name__ == "__main__":
-    verify_setup()
+# Interview | Education, Experience
+cpd_interview = TabularCPD(
+variable=&#39;Interview&#39;,
+variable_card=2,
+values=[
+[0.90, 0.70, 0.60, 0.30], # Good
+[0.10, 0.30, 0.40, 0.70] # Bad
+],
+evidence=[&#39;Education&#39;, &#39;Experience&#39;],
+evidence_card=[2, 2],
+state_names={
+&#39;Interview&#39;: [&#39;Good&#39;, &#39;Bad&#39;],
+&#39;Education&#39;: [&#39;High&#39;, &#39;Low&#39;],
+&#39;Experience&#39;: [&#39;Experienced&#39;, &#39;Inexperienced&#39;]
+}
+)
+# HiringDecision | Interview
+cpd_hiring = TabularCPD(
+variable=&#39;HiringDecision&#39;,
+variable_card=2,
+values=[
+[0.85, 0.20], # Hired
+[0.15, 0.80] # NotHired
+],
+evidence=[&#39;Interview&#39;],
+evidence_card=[2],
+state_names={
+&#39;HiringDecision&#39;: [&#39;Hired&#39;, &#39;NotHired&#39;],
+&#39;Interview&#39;: [&#39;Good&#39;, &#39;Bad&#39;]
+}
+)
+model.add_cpds(
+cpd_education,
+cpd_experience,
+cpd_interview,
+cpd_hiring
+)
+assert model.check_model()
+inference = VariableElimination(model)
+result = inference.query(
+variables=[&#39;HiringDecision&#39;],
+evidence={&#39;Education&#39;: &#39;High&#39;, &#39;Experience&#39;: &#39;Experienced&#39;}
+)
+print(result)
